a28eb3dec0fe5f9bd580b2862f7a9495ebb44650
Juan Vicentini Cordovil <jvicentinicordovil@gmail.com>
2026-05-16 13:50:36 -0300

    Remove agendamento functionality and associated styles from the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import 'dotenv/config'
-import { router } from './routes/agendamentoRoute.js';
 import cors from "cors";
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors())
 
-app.use('/api/agendamento', router);
 app.listen(port, () => {
     console.log("Servidor ligado! na porta: ", port)
 })
